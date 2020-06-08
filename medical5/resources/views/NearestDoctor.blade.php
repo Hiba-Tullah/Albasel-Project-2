efd8d7580f034f7a11dce1ed73fe959579e3bb33
hiba <mahhayate@gmail.com>
2020-06-08 23:19:17 +0300

    test msg 6-8-2020

---
+++ medical5/resources/views/NearestDoctor.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.master')
+@section('content')
+
+
+
+
+<div class="row justify-content-center" style="padding-top:150px;padding-bottom: 150px ">
+        <div class="col-md-8">
+<h2>Nearest Doctor</h2>
+<p>text</p>
+<p>text.</p>
+</div></div>
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
